bcb9d8ae4c6929a716efaf4d535761d05fe7b387
최준영 <choi081517@gmail.com>
2026-03-26 19:22:29 +0900

    Pruduct Class 기능에 formatPrice() 메서드(1,200,000원 형태로 반환하는 메서드)를 추가

--- src/Product.java
+++ src/Product.java
@@ -18,21 +18,29 @@ public class Product {
     }
 
     // 3. 기능
+    // 가격을 1,200,000원 형태로 반환하는 메서드
+    private String formatPrice() {
+        return String.format("%,d원", price);
+    }
+
     // 상품 하나를 설명하는 메서드
     public void printInfo() {
         System.out.println("상품명: " + name);
-        System.out.println("가격: " + price + "원");
+        System.out.println("가격: " + formatPrice());
         System.out.println("설명: " + description);
         System.out.println("재고 수량: " + stock);
         System.out.println("---------------------");
     }
 
 
-    // 메뉴에 출력될 상품 한 줄 정보를 반환한다
+    // 메뉴에 출력될 상품 한 줄 정보를 반환하는 메서드
     public String toMenuLine() {
         // 가격을 1,200,000원 형태로 보기 좋게 포맷
         String formattedPrice = String.format("%,d원", price);
         return name + " | " + formattedPrice + " | " + description;
     }
 
-    }
+
+
+
+}
