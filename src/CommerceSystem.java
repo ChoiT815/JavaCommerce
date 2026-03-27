ff2319c1a8296985edb7014f7c038791815a858e
최준영 <choi081517@gmail.com>
2026-03-27 11:10:21 +0900

    step2 CommerceSystem Class 생성 및 Main Class의 입력 및 조회 기능을 CommerceSystem Class에서 구현하고 Main Class는 상품 리스트 준비와 시스템 시작만 구현

--- src/CommerceSystem.java
+++ src/CommerceSystem.java
@@ -1,3 +1,48 @@
+import java.util.List;
+import java.util.Scanner;
+
 public class CommerceSystem {
+      // 시스템이 관리할 상품 목록 (main에서 주입받음)
+      private final List<Product> products;
+
+      // 사용자 입력 도구 (start()에서 반복 사용)
+      private final Scanner scanner;
+
+
+    public CommerceSystem(List<Product> products) {
+        this.products = products;  // main에서 만든 리스트를 그대로 받는다.
+        this.scanner = new Scanner(System.in);
+
+    }
+
+    void start() {
+        while (true) {
+            printMenu();
+
+            System.out.print("번호를 입력하세요: ");
+            int choice = scanner.nextInt();
+
+            if (choice == 0) {
+                System.out.println("프로그램을 종료합니다.");
+                break;
+            }
+
+            if (choice < 1 || choice > products.size()) {
+                System.out.println("잘못된 번호입니다.");
+                continue;
+            }
+
+            Product selected = products.get(choice - 1);
+            selected.printInfo();
+
+        }
+        scanner.close();
+    }
+
+    private void printMenu() {
+
+    }
 
 }
+
+
--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,8 @@
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 
 public class Main {
-    static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
         List<Product> products = new ArrayList<>();
 
         Product laptop = new Product(
@@ -13,19 +11,19 @@ public class Main {
                 "LG 그램 17은 “17인치 대화면인데도 1.3kg대 초경량”을 실현한 노트북",
                 10
         );
-        Product Phone = new Product(
+        Product phone = new Product(
                 "Galaxy S26 Ultra 1TB",
                 2260000,
                 "갤럭시 S26 울트라는 ‘프라이버시 디스플레이 + AI 성능 강화 + 200MP 카메라’를 핵심으로 한 삼성의 2026년형 최상위 플래그십 스마트폰",
                 10
         );
-        Product Earphones = new Product(
+        Product earphones = new Product(
                 "Galaxy Buds4 Pro",
                 359000,
                 "갤럭시 버즈 시리즈의 프로급 무선 이어폰으로, ANC(적응형 액티브 노이즈 캔슬링) 성능과 음질, 방수 등급이 기본형 버즈4보다 강화된 모델",
                 10
         );
-        Product Headset = new Product(
+        Product headset = new Product(
                 "Apple 2024 AirPods Max Noise Cancelling Bluetooth Headphones",
                 613490,
                 "Apple이 출시한 액티브 노이즈 캔슬링을 지원하는 블루투스 헤드폰",
@@ -33,33 +31,12 @@ public class Main {
         );
 
         products.add(laptop);
-        products.add(Phone);
-        products.add(Earphones);
-        products.add(Headset);
+        products.add(phone);
+        products.add(earphones);
+        products.add(headset);
 
-
-        System.out.println("[ 실시간 커머스 플랫폼 - 전자제품 ]");
-
-    while (true) {
-        for (int i = 0; i < products.size(); i++) {
-            System.out.println((i + 1) + ". " + products.get(i).toMenuLine());
-        }
-
-        System.out.println("0. 종료 | 프로그램 종료");
-
-        System.out.print("번호를 입력하세요: ");
-        int choice = scanner.nextInt();
-
-        if (choice == 0) {
-            System.out.println("프로그램을 종료합니다.");
-            break;
-        } else {
-            Product selected = products.get(choice - 1);
-            selected.printInfo();
-        }
-
-    }
-        scanner.close();
+        CommerceSystem system = new CommerceSystem(products);
+        system.start();
     }
 
 }
